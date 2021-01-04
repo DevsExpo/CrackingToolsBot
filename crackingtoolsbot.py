fb89a8028430c22c7707e4f05d38039f45fbca0a
スタークギャング <StarkGangZ@gmail.com>
2021-01-04 09:18:18 +0530

    Update crackingtoolsbot.py

--- crackingtoolsbot.py
+++ crackingtoolsbot.py
@@ -5,7 +5,7 @@ from telethon import TelegramClient, events, functions, Button
 from telethon.tl.functions.users import GetFullUserRequest
 from loggers import logging
 from BotConfig import Config
-from sql_s.broadcast_sql import add_usersid_in_db, already_added, get_all_users
+from .sql_s.broadcast_sql import add_usersid_in_db, already_added, get_all_users
 
 bot = TelegramClient("bot", api_id=Config.API_ID, api_hash=Config.API_HASH)
 UltraBot = bot.start(bot_token=Config.BOT_TOKEN)
